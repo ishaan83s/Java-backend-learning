d8db8db3b677cc62356880ca5649344f1c9a4f63
ishaan83s <berealishaan@gmail.com>
2026-07-11 11:31:32 +0530

    Update

--- day 2/src/main/java/SpringMVC/Controller/EmployeeController.java
+++ day 2/src/main/java/SpringMVC/Controller/EmployeeController.java
@@ -1,14 +1,28 @@
 package SpringMVC.Controller;
 
+import SpringMVC.Entity.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
 public class EmployeeController {
+    //Temporary Database
+    private List<Employee>employeeList = newArrayList<>();
     public String registerEmployee()
 
     public List<Employee>getAllEmployees()
+        @GetMapping({"/id"})
+        public Employee getEmployeeById(@PathVariable int id){
+            for(Employee emp:employeeList){
+                if(emp.getId() == id){
+                    return emp;
+                }
+            }
+            return null;
+        }
+
     public Employee getEmployeeById()
     public Employee setEmployee()
     public String updateEmployee()

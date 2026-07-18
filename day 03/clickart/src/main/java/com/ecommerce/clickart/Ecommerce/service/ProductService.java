2f7c70db8048c351c48cec734f28bd3be9a4e739
ishaan83s <berealishaan@gmail.com>
2026-07-18 10:02:50 +0530

    Added read all in productservice

--- day 03/clickart/src/main/java/com/ecommerce/clickart/Ecommerce/service/ProductService.java
+++ day 03/clickart/src/main/java/com/ecommerce/clickart/Ecommerce/service/ProductService.java
@@ -5,21 +5,41 @@ import com.ecommerce.clickart.Ecommerce.entity.ProductEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.*;
+import java.util.List;
 
 @Service
 public class ProductService {
-    //DUMMY DATABASE
-    private List<ProductEntity>productList = new ArrayList<>();
+    // DUMMY DATABASE
+    private List<ProductEntity> productList = new ArrayList<>();
 
+    // CREATE
     public ProductDTO addProduct(ProductDTO dto) {
         ProductEntity entity = new ProductEntity();
         entity.setId(dto.getId());
         entity.setName(dto.getName());
         entity.setPrice(dto.getPrice());
-        entity.setQuantity((dto.getQuantity()));
+        entity.setQuantity(dto.getQuantity());
 
+        // db call
         productList.add(entity);
         return dto;
+    }
+
+    // READ ALL
+    public List<ProductDTO> getAllProduct() {
+        List<ProductDTO> dtoList = new ArrayList<>();
+
+        // Loop through all entities, convert them to DTOs, and add to the list
+        for (ProductEntity entity : productList) {
+            ProductDTO dto = new ProductDTO();
+            dto.setId(entity.getId());
+            dto.setName(entity.getName());
+            dto.setPrice(entity.getPrice());
+            dto.setQuantity(entity.getQuantity());
+
+            dtoList.add(dto);
+        }
+
+        return dtoList;
     }
 }
